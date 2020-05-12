1b969218d0f300bc065cd19dd83edf5e912ca07b
Sheshesi <alexkadyrbayev@gmail.com>
2020-05-12 21:11:46 +0700

    Reading figures fro file

--- src/main.c
+++ src/main.c
@@ -2,7 +2,23 @@
 #include "inputCheck.h"
 #include "print.h"
 #include <stdio.h>
+#define MAX_LEN 100
 int main()
 {
+    char** data = (char**)malloc(MAX_LEN * sizeof(char*));
+    for (int k = 0; k < MAX_LEN; k++) {
+        data[k] = (char*)malloc(MAX_LEN * sizeof(char));
+    }
+    for (int i = 0; i < MAX_LEN; i++) {
+        for (int j = 0; j < MAX_LEN; j++) {
+            data[i][j] = 0;
+        }
+    }
+    data = file_func(data);
+    for (int i = 0; i < MAX_LEN; i++) {
+        for (int j = 0; j < MAX_LEN; j++) {
+            printf("%s\n", data[i]);
+        }
+    }
     return 0;
 }
