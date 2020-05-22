6ab06b0a278a5db7a77ddbe57665939cafc7cc5c
Sheshesi <alexkadyrbayev@gmail.com>
2020-05-22 20:00:09 +0700

    Rewriting function.cpp

--- src/function.cpp
+++ src/function.cpp
@@ -1,29 +1,12 @@
 #include "function.h"
-#include "print.h"
-#include <stdio.h>
-char** file_func(char** data)
+float S(float r)
 {
-    FILE* myfile;
-    myfile = fopen("./example/figures.txt", "r");
-    if (myfile == NULL) {
-        printf("File open error!");
-        return 0;
-    }
-    int i = 0;
-    char *temp, *ptrFile, *buff;
-    while (i < 100) {
-        temp = (char*)malloc(1000 * sizeof(char));
-        ptrFile = fgets(temp, 1000, myfile);
-        if (ptrFile == NULL) {
-            if (feof(myfile) != 0) {
-                break;
-            } else {
-                continue;
-            }
-        }
-        data[i] = temp;
-        i++;
-    }
-    fclose(myfile);
-    return data;
+    float s = pi * pow(r, 2);
+    return s;
+}
+
+float P(float r)
+{
+    float p = 2 * pi * r;
+    return p;
 }
--- src/function.h
+++ src/function.h
@@ -1,4 +1,9 @@
 #ifndef FUNCTION_H
 #define FUNCTION_H
-char** file_func(char** data);
+#include <math.h>
+#define pi 3.14
+
+float S(float r);
+float P(float r);
+
 #endif
